685a54cf92a0b20771ed7f0d91a5414cacc8f8c6
Erick Mata Torres <abcdefghij110@outlook.com>
2023-12-24 16:17:39 -0700

    Declarativo avanzado

--- TestSoftwareDeveloper/Models/ConexionSql.cs
+++ TestSoftwareDeveloper/Models/ConexionSql.cs
@@ -6,8 +6,8 @@ namespace TestSoftwareDeveloper.Models
     {
         public static SqlConnection Conectar()
         {
-            //string conx = "DATA SOURCE = A; INITIAL CATALOG = TestSoftwareDeveloper; INTEGRATED SECURITY = YES";
-            string conx = "SERVER = TestSoftwareDeveloper.mssql.somee.com; DATABASE = TestSoftwareDeveloper; USER ID = Abcdefghij110_SQLLogin_1; PASSWORD = jrhjvpetvh;";
+            string conx = "DATA SOURCE = A; INITIAL CATALOG = TestSoftwareDeveloper; INTEGRATED SECURITY = YES";
+            //string conx = "SERVER = TestSoftwareDeveloper.mssql.somee.com; DATABASE = TestSoftwareDeveloper; USER ID = Abcdefghij110_SQLLogin_1; PASSWORD = jrhjvpetvh;";
             SqlConnection s=new SqlConnection(conx);
             return s;
         }
--- TestSoftwareDeveloper/wwwroot/js/Url.js
+++ TestSoftwareDeveloper/wwwroot/js/Url.js
@@ -1,2 +1,2 @@
-//const Url = "https://localhost:7263";
-const Url = "http://practicar.somee.com";
+const Url = "https://localhost:7263";
+//const Url = "http://practicar.somee.com";
